76050ddda239d2abefb2cb9722429431e1cd6713
BerkCapaci <berkcapaci@hotmail.com.tr>
2026-05-12 21:57:00 +0200

    photos upload.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const photoSources = [ 
-
-]
+let photoSource = [ 
+"photo_01.jpg"
+"photo_02.jpg"
+"photo_03.png"
+"photo_04.jpg"
+"photo_05.jpg"
+"photo_06.jpg"
+"photo_07.jpg"
+"photo_08.jpg"
+"photo_09.jpg"
+"photo_10.jpg"
+"photo_11.jpg"
+"photo_12.jpg"
+];
 
 function renderPhoto() {
     for (let i = 1; i <= 12; i++) {
