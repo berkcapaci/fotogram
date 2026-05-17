04f851571af2ee15c470eae0ceb00da697322fdb
BerkCapaci <berkcapaci@hotmail.com.tr>
2026-05-17 19:40:25 +0200

    web page without footer

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let currentPhotoIndex = 0;
+
 let photoSource = [ 
 "photo_01.jpg",
 "photo_02.jpg",
@@ -14,14 +16,69 @@ let photoSource = [
 ];
 
 function renderPhoto() {
+    let gallery = document.getElementById('photo_gallery');
+        gallery.innerHTML = "";
     for (let i = 0; i < photoSource.length; i++) {
-        
-        document.getElementById('photo_gallery').innerHTML += createPhoto(i);
+        gallery.innerHTML += createPhoto(i);
     }
+    initDialog();
 }
 
 function createPhoto(i) {
-    return ` <div id= "photo">
-        <img src="./assets/images/${photoSource[i]}" alt="Photo ${i+1}">
-        </div> `;
+       return `
+        <div class="photo">
+            <img
+                src="./assets/images/${photoSource[i]}" alt="Photo ${i + 1}"
+                onclick="openDialog(${i})">
+        </div>
+    `;
+}
+
+function openDialog(i) {
+    currentPhotoIndex = i;
+    updateDialog();
+    document.getElementById('photo_dialog').showModal();
+    }
+  
+function updateDialog() {    
+    document.getElementById('dialog_image').src =
+        `./assets/images/${photoSource[currentPhotoIndex]}`;
+    document.getElementById('dialog_title').innerText = photoSource[currentPhotoIndex];    
+    document.getElementById('photo_counter').innerText = `${currentPhotoIndex + 1} / ${photoSource.length}` ;
+}
+
+function nextPhoto() {
+    currentPhotoIndex++;
+    if (currentPhotoIndex >= photoSource.length) {
+        currentPhotoIndex = 0;
+    }
+    updateDialog();
+}
+
+function previousPhoto() {
+    currentPhotoIndex--;
+    if (currentPhotoIndex < 0) {
+        currentPhotoIndex = photoSource.length - 1;
+    }
+    updateDialog();
+}
+
+function closeDialog() {
+
+    document.getElementById('photo_dialog').close();
+}
+
+function initDialog() {
+    const dialog = document.getElementById('photo_dialog');
+    dialog.addEventListener('click', (event) => {
+        const dialogDimensions = dialog.getBoundingClientRect();
+        if (
+            event.clientX < dialogDimensions.left ||
+            event.clientX > dialogDimensions.right ||
+            event.clientY < dialogDimensions.top ||
+            event.clientY > dialogDimensions.bottom
+        ) {
+            dialog.close();
+        }
+    });
 }
